bdc050b2ba6a18956321ac0f3cba7e56fb236a6d
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2020-08-05 16:55:00 +0100

    Lol nice form m8

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Background>
+      <Paper>
+        <Stack>
+          <TextField id="name" label="Name" variant="outlined" />
+          <TextField id="email" label="Email" variant="outlined" />
+          <TextField id="password" label="Password" variant="outlined" />
+          <TextField
+            id="confirm-password"
+            label="Confirm Password"
+            variant="outlined"
+          />
+        </Stack>
+      </Paper>
+    </Background>
   );
 }
+
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: #f5f5f5;
+`;
+
+const Stack = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export default App;
